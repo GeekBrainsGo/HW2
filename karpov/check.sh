2e94440f3b23743e8a419d05261476a8a25382b9
Artem K <art.frela@gmail.com>
2019-09-16 01:07:05 +0300

    add check case

--- karpov/check.sh
+++ karpov/check.sh
@@ -10,3 +10,7 @@ curl -X POST 127.0.0.1:3333/search -H 'Content-type: application/json' \
 # wrong header
 curl -X POST 127.0.0.1:3333/search -H 'Content-type: application/xml' \
 -d '{"search":"Чак Норрис", "sites":["https://ru.wikipedia.org/wiki/Норрис,_Чак","https://ru.wikipedia.org/wiki/Крутой_Уокер:_Правосудие_по-техасски","https://yandex.ru"]}'
+
+# wrong payload
+curl -X POST 127.0.0.1:3333/search -H 'Content-type: application/json' \
+-d '"Чак Норрис", "https://ru.wikipedia.org/wiki/Норрис,_Чак","https://ru.wikipedia.org/wiki/Крутой_Уокер:_Правосудие_по-техасски","https://yandex.ru"'
